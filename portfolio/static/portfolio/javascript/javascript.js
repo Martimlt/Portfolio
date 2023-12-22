ff1b36166721e55cc04e661512f9f203acb9d6a5
mateixeira <mateixeira@ucsd.edu>
2023-12-22 11:00:20 -0800

    Alterações

--- portfolio/static/portfolio/javascript/javascript.js
+++ portfolio/static/portfolio/javascript/javascript.js
@@ -11,3 +11,15 @@ function myFunction() {
       x.className = "topnav";
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetch('https://api.ipma.pt/open-data/forecast/meteorology/cities/daily/1110600.json')// envia GET request ao URL
+  .then(response => response.json()) // põe os dados na consola
+  .then(data => {
+      const tempMax = data.data[0].tMax;
+      const tempMin = data.data[0].tMin
+      document.getElementById('tempMax').innerHTML = tempMax
+      document.getElementById('tempMin').innerHTML = tempMin
+  });
+  return false;
+});
