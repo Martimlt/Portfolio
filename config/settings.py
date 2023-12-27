d4d9fccd0c8893693939d71d7dc11040091503c7
mateixeira <mateixeira@ucsd.edu>
2023-12-27 10:27:45 -0800

    More changes

--- config/settings.py
+++ config/settings.py
@@ -120,8 +120,8 @@ STATIC_URL = '/portfolio/static/'
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 
 # Media files
-MEDIA_URL = '/portfolio/'
-MEDIA_ROOT = BASE_DIR / 'portfolio/'
+MEDIA_URL = '/portfolio/images/'
+MEDIA_ROOT = BASE_DIR / 'portfolio/images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
