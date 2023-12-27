694d1019eb1ad76d13ef67265946a06439ce2526
mateixeira <mateixeira@ucsd.edu>
2023-12-27 10:03:30 -0800

    More changes

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = 'portfolio'
 
@@ -8,3 +10,6 @@ urlpatterns = [
     path('ucsd', views.ucsd_page_view, name='ucsd'),
     path('lusofona', views.lusofona_page_view, name='lusofona'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
