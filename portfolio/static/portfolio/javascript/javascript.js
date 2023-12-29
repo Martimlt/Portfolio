ac84084835bef3c0b08cff6e35fe944f54913229
mateixeira <mateixeira@ucsd.edu>
2023-12-29 07:58:43 -0800

    Done

--- portfolio/static/portfolio/javascript/javascript.js
+++ portfolio/static/portfolio/javascript/javascript.js
@@ -1,21 +1,23 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
+document.addEventListener('DOMContentLoaded', function () {
+  function toggleMenu() {
+      var x = document.getElementById("myTopnav");
+      if (x.className === "topnav") {
+          x.className += " responsive";
+      } else {
+          x.className = "topnav";
+      }
   }
-}
 
-// Function to close the dropdown menu
-function closeMenu() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav responsive") {
-    x.className = "topnav";
+  document.querySelector('.topnav .icon').addEventListener('click', toggleMenu);
+
+  function closeMenu() {
+      var x = document.getElementById("myTopnav");
+      if (x.className.includes("responsive")) {
+          x.className = "topnav";
+      }
   }
-}
 
-// Add event listeners to each menu item
-document.querySelectorAll('.topnav a').forEach(item => {
-  item.addEventListener('click', closeMenu);
+  document.querySelectorAll('.topnav a:not(.icon)').forEach(item => {
+      item.addEventListener('click', closeMenu);
+  });
 });
