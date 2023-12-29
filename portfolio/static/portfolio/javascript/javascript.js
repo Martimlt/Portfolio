aad0259b118f3a1584d5b6cfa8ae966f66880c96
mateixeira <mateixeira@ucsd.edu>
2023-12-29 07:51:43 -0800

    Done

--- portfolio/static/portfolio/javascript/javascript.js
+++ portfolio/static/portfolio/javascript/javascript.js
@@ -1,25 +1,21 @@
-document.getElementById('hamburger').addEventListener('click', function() {
-    var navLinks = document.getElementById('navLinks');
-    navLinks.classList.toggle('show');
-});
-
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetch('https://api.ipma.pt/open-data/forecast/meteorology/cities/daily/1110600.json')// envia GET request ao URL
-  .then(response => response.json()) // põe os dados na consola
-  .then(data => {
-      const tempMax = data.data[0].tMax;
-      const tempMin = data.data[0].tMin
-      document.getElementById('tempMax').innerHTML = tempMax
-      document.getElementById('tempMin').innerHTML = tempMin
-  });
-  return false;
+// Function to close the dropdown menu
+function closeMenu() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav responsive") {
+    x.className = "topnav";
+  }
+}
+
+// Add event listeners to each menu item
+document.querySelectorAll('.topnav a').forEach(item => {
+  item.addEventListener('click', closeMenu);
 });
